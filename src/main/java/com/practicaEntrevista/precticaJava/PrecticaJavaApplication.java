1f3edcfe09c1e8c611e65273370337abcb0616a8
EstebanAndresPetrizzo <66917268+EstebanAndresPetrizzo@users.noreply.github.com>
2023-08-06 16:05:20 -0300

    Commit inicial

---
+++ src/main/java/com/practicaEntrevista/precticaJava/PrecticaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.practicaEntrevista.precticaJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecticaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecticaJavaApplication.class, args);
+	}
+
+}
